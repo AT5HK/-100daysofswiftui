98b7c303d89372461e2da3a276f8e650499631e9
Auston <auston.salvana@live.com>
2022-01-24 20:07:52 -0800

    swiftui button with alert message

---
+++ GuessTheFlag/ButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by auston salvana on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Please read this")
+        }
+        
+    }
+    
+    func executeDelete() {
+        print("delete now")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
